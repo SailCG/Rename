c6af69e5cb53f214750f200324f692baf43d4b4c
SailCG <66212353+SailCG@users.noreply.github.com>
2020-11-19 18:12:04 +0800

    Create rename.py

---
+++ rename.py
@@ -0,0 +1,18 @@
+# _*_ coding: UTF-8 _*_
+import os,shutil,re,glob
+#文件目录
+path =r'F:\油管'
+file_filter=r"*.mp4"
+#要替换的字符串
+strToReplace="y2mate.com - "
+#替换成的字符串
+replace=""
+
+filelist = os.listdir(path) #该文件夹下所有的文件（包括文件夹）
+os.chdir(path)
+for temp in glob.glob(file_filter):
+    (filename,extension) = os.path.splitext(temp)
+    newName=re.sub(strToReplace,replace,filename)+extension
+    os.rename(temp,newName)#重命名
+print("-----完成-----")
+
